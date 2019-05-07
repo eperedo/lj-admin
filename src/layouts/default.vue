637d1793d21447620c9f26bb068e8cfcce38489b
Eduardo P. Rivero <jrperedo@gmail.com>
2019-05-07 12:07:53 -0500

    chore(build): Initial nuxt configuration

---
+++ src/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h1>Default Layout</h1>
+    <nuxt-child/>
+  </section>
+</template>
+
+<script>
+export default {
+	name: 'layouts-default',
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
