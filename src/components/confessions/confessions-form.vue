2f2116ae035b4dbb1f7c90e31ecc95c7d125857a
Eduardo P. Rivero <jrperedo@gmail.com>
2019-05-08 00:38:20 -0500

    feat(web): Added ability to register confessions

---
+++ src/components/confessions/confessions-form.vue
@@ -0,0 +1,190 @@
+<template>
+  <v-form @submit.prevent="onSubmit">
+    <v-layout wrap mt-3>
+      <v-flex xs12>
+        <p class="headline">{{ title }}</p>
+      </v-flex>
+      <v-flex xs12>
+        <v-combobox
+          v-model="author"
+          item-value="id"
+          item-text="name"
+          label="Quién hizo la declaración?"
+          :items="characters"
+        ></v-combobox>
+      </v-flex>
+      <v-flex xs12>
+        <v-combobox
+          v-model="involved"
+          item-value="id"
+          item-text="name"
+          label="Quién está involucrado en la declaración?"
+          :items="characters"
+        ></v-combobox>
+      </v-flex>
+      <v-flex xs12>
+        <base-input v-model="when" label="Fecha" placeholder="YYYY-MM-DD" mask="####-##-##"/>
+      </v-flex>
+      <v-flex xs12>
+        <base-input
+          label="Dinero"
+          placeholder="La declaración incluye monto de coima?"
+          type="number"
+          v-model="money"
+        />
+      </v-flex>
+      <v-flex xs12>
+        <base-input
+          label="Fuente"
+          placeholder="URL como fuente de la declaración (noticia, imagen, otro documento)"
+          v-model="link"
+        />
+      </v-flex>
+      <v-flex xs12>
+        <base-input
+          label="Texto"
+          placeholder="Descripción corta de la declaración (140 caractéres)"
+          v-model="shortText"
+        />
+      </v-flex>
+      <v-flex xs12>
+        <v-textarea label="Texto" placeholder="Declaración completa" v-model="longText"/>
+      </v-flex>
+      <v-flex xs12 center>
+        <base-button color="primary" round type="submit" :loading="disableButton">Guardar</base-button>
+        <base-button color="error" round @click.native="clearForm">Cancelar</base-button>
+      </v-flex>
+    </v-layout>
+  </v-form>
+</template>
+
+<script>
+const initialData = {
+	author: null,
+	involved: null,
+	money: 0,
+	link: null,
+	longText: '',
+	shortText: '',
+	when: null,
+};
+
+function data() {
+	return {
+		...initialData,
+	};
+}
+
+function authorId() {
+	return this.author ? this.author.id : null;
+}
+
+function clearForm() {
+	this.involved = this.characters[1];
+	this.money = 0;
+	this.link = '';
+	this.longText = '';
+	this.shortText = '';
+	this.when = null;
+	this.$emit('clear');
+}
+
+function created() {
+	const barata = this.characters.findIndex(c => c.name.includes('Barata'));
+	if (barata > -1) {
+		this.author = this.characters[barata];
+	}
+	this.involved = this.characters[1];
+}
+
+function clearHandler(newVal) {
+	this.clearForm();
+}
+
+function involvedId() {
+	return this.involved ? this.involved.id : null;
+}
+
+function links() {
+	return this.link
+		? {
+				[this.link]: {
+					type: 'link',
+					link: this.link,
+				},
+		  }
+		: '';
+}
+
+function modelHandler(newVal) {
+	if (newVal) {
+		this.author = this.model.author;
+		this.involved = this.model.involved;
+		this.link = this.model.link;
+		this.longText = this.model.longText;
+		this.money = this.model.money;
+		this.shortText = this.model.shortText;
+		this.when = this.model.when;
+	}
+}
+
+function onSubmit() {
+	const confession = {
+		authorId: this.authorId,
+		involvedId: this.involvedId,
+		links: this.links,
+		longText: this.longText,
+		money: this.money,
+		shortText: this.shortText,
+		when: this.when,
+	};
+	this.$emit('submit', confession);
+}
+
+export default {
+	name: 'confessions-form',
+	created,
+	computed: {
+		authorId,
+		involvedId,
+		links,
+	},
+	data,
+	props: {
+		disableButton: {
+			default: false,
+			type: Boolean,
+		},
+		characters: {
+			default: () => [],
+			type: Array,
+		},
+		clear: {
+			default: false,
+			type: Boolean,
+		},
+		model: {
+			default: () => null,
+			type: Object,
+		},
+		title: {
+			default: '',
+			type: String,
+		},
+	},
+	methods: {
+		clearForm,
+		onSubmit,
+	},
+	watch: {
+		clear: {
+			handler: clearHandler,
+		},
+		model: {
+			deep: true,
+			handler: modelHandler,
+			immediate: true,
+		},
+	},
+};
+</script>
--- src/pages/confessions/index.vue
+++ src/pages/confessions/index.vue
@@ -1,32 +1,102 @@
 <template>
   <ApolloQuery
-    :query="require('@/graphql/confessions/list.gql')"
+    :query="require('@/graphql/characters/list.gql')"
     :variables="{
 			limit,
 			offset,
 		}"
   >
-    <template v-slot="{ result: { loading, error, data } }">
-      <confessions-list v-if="data" :rows="data.confessions"></confessions-list>
+    <template v-slot="{ result: { data: characters } }">
+      <ApolloQuery
+        :query="require('@/graphql/confessions/list.gql')"
+        :variables="{
+					limit,
+					offset,
+				}"
+      >
+        <template v-slot="{ result: { loading, error, data } }">
+          <confessions-form
+            v-if="status && characters"
+            :characters="characters.characters"
+            :clear="clearForm"
+            :model="confession"
+            @clear="hideForm"
+            @submit="onSubmit"
+          ></confessions-form>
+          <confessions-list v-if="data" :rows="data.confessions" @add="add"></confessions-list>
+        </template>
+      </ApolloQuery>
     </template>
   </ApolloQuery>
 </template>
 
 <script>
+import { FORM_ADD_STATUS } from '../../components/characters/helper';
+import confessionsForm from '@/components/confessions/confessions-form.vue';
 import confessionsList from '@/components/confessions/confessions-list.vue';
 
 function data() {
 	return {
+		clearForm: false,
+		confession: {
+			authorId: null,
+			involvedId: null,
+			money: 30,
+			link: null,
+			longText: '',
+			shortText: '',
+			when: null,
+		},
 		limit: 50,
 		offset: 0,
+		status: null,
 	};
+}
+
+function add() {
+	this.status = FORM_ADD_STATUS;
+}
+
+function hideForm() {
+	this.status = null;
+}
+
+async function onSubmit(confession) {
+	await this.$apollo.mutate({
+		mutation: require('@/graphql/confessions/insert.gql'),
+		update: this.update,
+		variables: {
+			...confession,
+		},
+	});
+	this.clearForm = !this.clearForm;
+}
+
+function update(store, { data: { insert_confessions } }) {
+	const query = {
+		query: require('@/graphql/confessions/list.gql'),
+		variables: {
+			limit: this.limit,
+			offset: this.offset,
+		},
+	};
+	const data = store.readQuery(query);
+	data.confessions.push(insert_confessions.returning[0]);
+	store.writeQuery({ ...query, data });
 }
 
 export default {
 	name: 'pages-confessions',
 	components: {
+		confessionsForm,
 		confessionsList,
 	},
 	data,
+	methods: {
+		add,
+		hideForm,
+		onSubmit,
+		update,
+	},
 };
 </script>
